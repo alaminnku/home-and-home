e8c0047ec0d5ccff229fb4a795d30c54f46aee89
sohan dharmarajah <sohan@planpod.com.au>
2022-09-14 05:26:45 +0530

    Updated root domains to next.config.js

--- next.config.js
+++ next.config.js
@@ -8,6 +8,15 @@ const nextConfig = {
       "media.istockphoto.com",
       "scontent-tpe1-1.cdninstagram.com",
       "scontent-iad3-2.cdninstagram.com",
+      "instagram.fcrk1-3.fna.fbcdn.net",
+      "instagram.ftru2-1.fna.fbcdn.net",
+      "instagram.ftru2-3.fna.fbcdn.net",
+      "instagram.fcrk1-2.fna.fbcdn.net",
+      "instagram.faqp1-1.fna.fbcdn.net",
+      "instagram.fcrk1-5.fna.fbcdn.net",
+      "instagram.ftru2-2.fna.fbcdn.net",
+      "instagram.fczl1-2.fna.fbcdn.net",
+      "scontent-ort2-1.cdninstagram.com"
     ],
   },
 };
