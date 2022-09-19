fa9f42ec779dcc7849919de6e962aa9150560120
sohan dharmarajah <sohan@planpod.com.au>
2022-09-19 05:54:18 +0530

    Minor text change

--- components/home/Hero.jsx
+++ components/home/Hero.jsx
@@ -3,7 +3,7 @@ import styles from "@styles/home/Hero.module.css";
 export default function Hero() {
   return (
     <section className={styles.hero}>
-      <h1>Welcome to our application</h1>
+      <h1>Welcome to Home and Home</h1>
     </section>
   );
 }
