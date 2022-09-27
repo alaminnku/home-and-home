d4845ebf1a66b482d5403976c21c067cf899f1ed
sohan dharmarajah <sohan@planpod.com.au>
2022-09-27 05:33:04 +0530

    Added MSFT Clarity tracking

--- pages/_document.js
+++ pages/_document.js
@@ -17,6 +17,17 @@ class CustomDocument extends Document {
           <meta property="og:locale" content="en_US" />
           <meta property="og:site_name" content="title" />
           <meta name="keywords" content="keywords" />
+
+          {/* MSFT Clarify tracking */}
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `(function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+              })(window, document, "clarity", "script", "dtuwyaz2yk");`,
+            }}
+            />;
         </Head>
 
         <body>
