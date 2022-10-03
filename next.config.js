cf97f58f8b4494a21f5eccc69003c6943bde42ae
sohan dharmarajah <sohan@planpod.com.au>
2022-10-03 08:26:03 +0530

    Updated next.config.js for mamaspot demo images

--- next.config.js
+++ next.config.js
@@ -5,22 +5,14 @@ const nextConfig = {
   swcMinify: true,
   images: {
     domains: [
-      "media.istockphoto.com",
-      "scontent-tpe1-1.cdninstagram.com",
-      "scontent-iad3-2.cdninstagram.com",
-      "instagram.fcrk1-3.fna.fbcdn.net",
-      "instagram.ftru2-1.fna.fbcdn.net",
-      "instagram.ftru2-3.fna.fbcdn.net",
-      "instagram.fcrk1-2.fna.fbcdn.net",
-      "instagram.faqp1-1.fna.fbcdn.net",
-      "instagram.fcrk1-5.fna.fbcdn.net",
       "instagram.ftru2-2.fna.fbcdn.net",
       "instagram.fczl1-2.fna.fbcdn.net",
       "scontent-ort2-1.cdninstagram.com",
       "scontent-ord5-1.cdninstagram.com",
       "scontent-lga3-1.cdninstagram.com",
       "www.budgetbytes.com", 'instagram.fsgn5-12.fna.fbcdn.net', 'scontent-sin6-4.cdninstagram.com', 'instagram.ftas1-1.fna.fbcdn.net', 'instagram.fzyl2-1.fna.fbcdn.net', 'scontent-atl3-1.cdninstagram.com', 'instagram.fmnl4-6.fna.fbcdn.net', 'instagram.fsgn5-14.fna.fbcdn.net', 'instagram.fcxj5-1.fna.fbcdn.net', 'scontent-dfw5-2.cdninstagram.com', 'instagram.fric1-1.fna.fbcdn.net', 'instagram.fsgn5-6.fna.fbcdn.net', 'instagram.fsgn5-8.fna.fbcdn.net', 'instagram.fsgn5-15.fna.fbcdn.net', 'scontent-dfw5-1.cdninstagram.com', 'scontent-sin6-1.cdninstagram.com', 'instagram.fmnl4-2.fna.fbcdn.net', 'scontent-sin6-3.cdninstagram.com', 'instagram.fmnl4-1.fna.fbcdn.net',
-      'cleanfoodcrush.com', 'instagram.fhan3-2.fna.fbcdn.net', 'scontent-lga3-2.cdninstagram.com', 'instagram.fhan3-5.fna.fbcdn.net', 'instagram.fhan3-1.fna.fbcdn.net', 'instagram.fhan3-4.fna.fbcdn.net', 'instagram.fhan4-1.fna.fbcdn.net', 'instagram.fbkk10-1.fna.fbcdn.net', 'instagram.fbkk14-1.fna.fbcdn.net', 'scontent-mad1-1.cdninstagram.com', 'instagram.fhan4-2.fna.fbcdn.net', 'instagram.fhan3-3.fna.fbcdn.net', 'scontent-hou1-1.cdninstagram.com', 'instagram.fhan4-3.fna.fbcdn.net'
+      'cleanfoodcrush.com', 'instagram.fhan3-2.fna.fbcdn.net', 'scontent-lga3-2.cdninstagram.com', 'instagram.fhan3-5.fna.fbcdn.net', 'instagram.fhan3-1.fna.fbcdn.net', 'instagram.fhan3-4.fna.fbcdn.net', 'instagram.fhan4-1.fna.fbcdn.net', 'instagram.fbkk10-1.fna.fbcdn.net', 'instagram.fbkk14-1.fna.fbcdn.net', 'scontent-mad1-1.cdninstagram.com', 'instagram.fhan4-2.fna.fbcdn.net', 'instagram.fhan3-3.fna.fbcdn.net', 'scontent-hou1-1.cdninstagram.com', 'instagram.fhan4-3.fna.fbcdn.net',
+      'www.justonecookbook.com', 'scontent-lhr8-2.cdninstagram.com', 'instagram.fmnl17-1.fna.fbcdn.net', 'instagram.fmnl17-3.fna.fbcdn.net', 'scontent-lhr8-1.cdninstagram.com', 'instagram.fmnl17-5.fna.fbcdn.net', 'scontent-atl3-1.cdninstagram.com', 'scontent-waw1-1.cdninstagram.com', 'instagram.fybz1-1.fna.fbcdn.net', 'instagram.fmnl17-4.fna.fbcdn.net', 'instagram.fmnl17-2.fna.fbcdn.net'
     ],
   },
 };
